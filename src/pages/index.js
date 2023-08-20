5d2a245e9cb2b1ea6ae82b455a1ed367349c101b
Екатерина Ромащенко <129534521+EkaterinaRomachenko@users.noreply.github.com>
2023-08-20 17:24:36 +0200

    feat: popup change city and burger menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,37 @@
 import '../pages/index.css';
+
+
+const locationBtn = document.querySelector('.header__location-button');
+const popupChoiseCity = document.querySelector('.popup__choise-city');
+const popupBurgerMenu = document.querySelector('.popup__menu-burger');
+const burgerMenu = document.querySelector('.menu-burger-icon');
+// const btnBurgerChangeCity = document.querySelector('.burger-menu__button-location')
+
+//  Открытие popup
+function openPopup(popup){
+    popup.classList.add('popup_opened')
+}
+
+function closePopup(popup){
+  popup.classList.remove('popup_opened')
+}
+
+locationBtn.addEventListener('click',function(){
+    openPopup(popupChoiseCity)
+})
+
+burgerMenu.addEventListener('click', function(){
+  if(burgerMenu.classList.toggle('menu-burger-icon_active')){
+    openPopup(popupBurgerMenu)
+  }else{
+    closePopup(popupBurgerMenu)
+  }
+})
+
+
+// btnBurgerChangeCity.addEventListener('click', function () {
+//   closePopup(burgerMenu);
+//   openPopup(popupChoiseCity)
+// })
+
+
