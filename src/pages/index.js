9f49ba9afef02845ae40425d0c739a43bbe5d844
Trushko Dmitriy <trushka12512@yandex.ru>
2023-08-20 13:33:46 +0300

    gallery and merging with other sections ready

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,38 @@
-// import '../pages/index.css'
+import '../pages/index.css';
 document.querySelectorAll(".fest-images__grid").forEach((carousel) => {
     const items = carousel.querySelectorAll(".fest-images__image");
     const buttonsHtml = Array.from(items, () => {
-      return `<span class="carousel__button"></span>`;
+      return `<span class="fest-images__button"></span>`;
     });
   
     carousel.insertAdjacentHTML(
       "beforeend",
       `
-          <div class="carousel__nav">
+          <div class="fest-images__nav">
               ${buttonsHtml.join("")}
           </div>
       `
     );
   
-    const buttons = carousel.querySelectorAll(".carousel__button");
+    const buttons = carousel.querySelectorAll(".fest-images__button");
   
     buttons.forEach((button, i) => {
       button.addEventListener("click", () => {
         // un-select all the items
         items.forEach((item) =>
-          item.classList.remove("carousel__item--selected")
+          item.classList.remove("fest-images__image_selected")
         );
         buttons.forEach((button) =>
-          button.classList.remove("carousel__button--selected")
+          button.classList.remove("fest-images__button_selected")
         );
   
-        items[i].classList.add("carousel__item--selected");
-        button.classList.add("carousel__button--selected");
+        items[i].classList.add("fest-images__image_selected");
+        button.classList.add("fest-images__button_selected");
       });
     });
   
     // Select the first item on page load
-    items[0].classList.add("carousel__item--selected");
-    buttons[0].classList.add("carousel__button--selected");
+    items[0].classList.add("fest-images__image_selected");
+    buttons[0].classList.add("fest-images__button_selected");
   });
   
