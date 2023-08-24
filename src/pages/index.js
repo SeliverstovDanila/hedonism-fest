034a5c98c2a35a77c5c5ff1946904fdb8cdbac44
Svetlana Sunagatullina <Ssunagatullina@mail.ru>
2023-08-24 21:26:27 +0300

    fix: error page adaptive

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,22 +45,22 @@ const burgerMenu = document.querySelector('.menu-burger-icon');
 // const btnBurgerChangeCity = document.querySelector('.burger-menu__button-location')
 
 //  Открытие popup
-function openPopup(popup){
-    popup.classList.add('popup_opened')
+function openPopup(popup) {
+  popup.classList.add('popup_opened')
 }
 
-function closePopup(popup){
+function closePopup(popup) {
   popup.classList.remove('popup_opened')
 }
 
-locationBtn.addEventListener('click',function(){
-    openPopup(popupChoiseCity)
+locationBtn.addEventListener('click', function () {
+  openPopup(popupChoiseCity)
 })
 
-burgerMenu.addEventListener('click', function(){
-  if(burgerMenu.classList.toggle('menu-burger-icon_active')){
+burgerMenu.addEventListener('click', function () {
+  if (burgerMenu.classList.toggle('menu-burger-icon_active')) {
     openPopup(popupBurgerMenu)
-  }else{
+  } else {
     closePopup(popupBurgerMenu)
   }
 })
