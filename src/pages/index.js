c507bfcd16283156913f6d4d28da994e815d3c43
Екатерина Ромащенко <129534521+EkaterinaRomachenko@users.noreply.github.com>
2023-08-29 18:28:28 +0200

    feat: click overlay and Escape, counter

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,13 +36,15 @@ document.querySelectorAll(".fest-images__grid").forEach((carousel) => {
   buttons[0].classList.add("fest-images__button_selected");
 });
 
-
+const popupOpenedSelector = 'popup_opened';
+const popupContainerSelector = 'popup__container'
 
 const locationBtn = document.querySelector('.header__location-button');
 
 const popupBurgerMenu = document.querySelector('.popup__menu-burger');
 const popupChoiseCity = document.querySelector('.popup__choise-city')
 const popupDonate = document.querySelector('.popup__donate-form');
+const popupBuyTickets = document.querySelector('.popup__buy-ticket');
 const burgerMenuBtn = document.querySelector('.menu-burger-icon');
 const btnChoiseCity = document.querySelector('.burger-menu__button-location')
 const btnChoiseCityBack = document.querySelector('.burger-menu__button-back')
@@ -51,8 +53,9 @@ const headerDropdown = document.querySelector('.header__dropdown')
 const headerText = document.querySelector('.header__text')
 const labelCity = document.querySelectorAll('.form-city__list');
 const btnDonate = document.querySelector('.btn__donate');
+const btnSupport = document.querySelector('.btn__support');
 const closeBtnDonate = document.querySelector('.popup__close-donate');
-
+const closeBtnTickets = document.querySelector('.popup__close-buy-ticket');
 //изменить город
 function changeCity(e) {
  if(headerText.textContent = e.target.closest('label').querySelector('span').textContent);
@@ -63,11 +66,15 @@ function changeCity(e) {
 
 //  Открытие popup
 function openPopup(popup){
-  popup.classList.add('popup_opened')
+  popup.classList.add(popupOpenedSelector)
+  popup.addEventListener('click', keyHandlerOverlay)
+  document.addEventListener('keydown', keyHandlerEsc)
 }
 
 function closePopup(popup){
-popup.classList.remove('popup_opened')
+  popup.classList.remove(popupOpenedSelector)
+  popup.removeEventListener('click', keyHandlerOverlay)
+  document.removeEventListener('keydown',  keyHandlerEsc)
 }
 
 // всплывающее окно
@@ -79,8 +86,8 @@ headerDropdown.classList.toggle('header__dropdown_active');
 
 burgerMenuBtn.addEventListener('click', function () {
 burgerMenuBtn.classList.toggle('menu-burger-icon_active')
-popupBurgerMenu.classList.toggle('popup_opened')
-if (popupChoiseCity.classList.contains('popup_opened')) {
+popupBurgerMenu.classList.toggle(popupOpenedSelector)
+if (popupChoiseCity.classList.contains(popupOpenedSelector)) {
   closePopup(popupChoiseCity);
   closePopup(popupBurgerMenu);
 }
@@ -96,7 +103,7 @@ closePopup(popupChoiseCity);
 openPopup(popupBurgerMenu);
 })
 
-btnDonate.addEventListener('click', function(){
+btnDonate & btnSupport.addEventListener('click', function(){
 openPopup(popupDonate)
 closePopup(popupBurgerMenu)
 })
@@ -104,6 +111,24 @@ closePopup(popupBurgerMenu)
 closeBtnDonate.addEventListener('click', function(){
 closePopup(popupDonate)
 })
+
+closeBtnTickets.addEventListener('click', function(){
+  closePopup(popupBuyTickets)
+})
+
+// Закрытие по нажатию на оверлей
+export function keyHandlerOverlay(evt) {
+  const popupActive = document.querySelector(`.${popupOpenedSelector}`)
+  if (!evt.target.closest(popupContainerSelector) && popupActive) closePopup(popupActive)
+}
+
+// Закрытие по нажатию на Esc
+export function  keyHandlerEsc(evt) {
+  if (evt.key === 'Escape') {
+      const popupActive = document.querySelector(`.${popupOpenedSelector}`)
+      popupActive && closePopup(popupActive)
+  }
+}
 
 
 import cardsArray from '../components/cards.json';
@@ -118,4 +143,25 @@ for (const cardElement of cardsArray) {
 }
 
 
+
+
+let btnMinus = document.querySelector('.controller_el_amount-minus');
+let btnPlus = document.querySelector('.controller_el_amount-plus');
+let amount = document.querySelector('.controller__amount-square');
+let sumTickets = document.querySelector('.popup__sum-ticket');
+
+// счетчик (покупка билетов)
+btnMinus.addEventListener('click', function(){
+    let result = Number(amount.textContent) - 1;
+    if(result > 0){
+      amount.textContent = result;
+      sumTickets.textContent = 500 * result + '₽';
+    }
+})
+
+btnPlus.addEventListener('click', function(){
+    let result = Number(amount.textContent) + 1;
+    amount.textContent = result;
+    sumTickets.textContent = 500 * result + '₽';
+})
 
