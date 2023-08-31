ececcc2b89092511446f3d2511738a66b4a52435
Екатерина Ромащенко <129534521+EkaterinaRomachenko@users.noreply.github.com>
2023-08-31 19:29:19 +0200

    feat: add constants

---
+++ src/components/constants.js
@@ -0,0 +1,25 @@
+export const cardTemplate = document.querySelector("#card-template").content;
+export const eventsCardsContainer = document.querySelector(".events__cards-container");
+export const locationBtn = document.querySelector(".header__location-button");
+
+export const popupBurgerMenu = document.querySelector('.popup__menu-burger');
+export const popupChoiseCity = document.querySelector('.popup__choise-city');
+export const popupDonate = document.querySelector('.popup__donate-form');
+export const closeButton = document.querySelector(".popup__close-cardform");
+export const burgerMenuBtn = document.querySelector(".header__menu-burger-icon");
+export const btnChoiseCity = document.querySelector(".popup__burger-menu-button-location");
+export const btnChoiseCityBack = document.querySelector(".popup__burger-menu-button-back");
+export const btnDonate = document.querySelector('.btn__donate');
+export const closeBtnDonate = document.querySelector('.popup__close-donate');
+export const closeBtnTickets = document.querySelector('.popup__close-buy-ticket');
+export const burgerCityText = document.querySelector(".popup__burger-menu-city-name");
+export const headerDropdown = document.querySelector(".header__dropdown");
+export const headerText = document.querySelector(".header__text");
+export const labelCity = document.querySelectorAll(".form-city__list");
+export const btnSupport = document.querySelector('.btn__support');
+export const btnMinus = document.querySelector('.controller_el_amount-minus');
+export const btnPlus = document.querySelector('.controller_el_amount-plus');
+export const amount = document.querySelector('.controller__amount-square');
+export const sumTickets = document.querySelector('.popup__sum-ticket');
+export const cardList = document.querySelectorAll(".card");
+export const popupOpenedSelector = 'popup_opened';
